bfbde1cfa4f23715e9c6219dd80ac8951c9f080b
unknown <swapnil.gajbhiye21@gmail.com>
2023-07-24 21:57:44 +0530

    Added the CollectionDemo file

---
+++ JulyCollectionDemo/src/CollectionDemo.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class CollectionDemo {
+
+	public static void main(String[] args) {
+		 
+		List<String> list = Arrays.asList("swapnil","pankaj","sangharsh","rahul");
+		
+		System.out.println(list);
+			
+		
+	}
+
+}
